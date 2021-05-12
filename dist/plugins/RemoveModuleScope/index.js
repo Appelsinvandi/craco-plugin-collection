59213d5890f87d390cdd5e4d6b9617cbb1e114a6
Builder <builder@workflow.github.com>
2021-05-12 14:28:41 +0000

    Build

---
+++ dist/plugins/RemoveModuleScope/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CracoRemoveModuleScopePlugin = void 0;
+var plugin_1 = require("./plugin");
+Object.defineProperty(exports, "CracoRemoveModuleScopePlugin", { enumerable: true, get: function () { return plugin_1.CracoRemoveModuleScopePlugin; } });
